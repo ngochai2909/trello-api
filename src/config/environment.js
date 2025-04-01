751f8958ecfbc11f9de734a84c4aa80acf230f0e
nguyenngochai2909 <nguyenngochai290902@gmail.com>
2025-04-01 15:03:32 +0700

    create route

--- src/config/environment.js
+++ src/config/environment.js
@@ -9,7 +9,7 @@ import 'dotenv/config'
 export const env = {
   MONGODB_URI: process.env.MONGODB_URI,
   DATABASE_NAME: process.env.DATABASE_NAME,
-  APP_PORT: process.env.PORT,
-  APP_HOST: process.env.HOSTNAME,
+  APP_PORT: process.env.APP_PORT,
+  APP_HOST: process.env.APP_HOST,
   AUTHOR: process.env.AUTHOR
 }
