9508303a1a1da74b87754ab53b30f2d5095ce529
nguyenngochai2909 <nguyenngochai290902@gmail.com>
2025-04-03 14:24:34 +0700

    api create board

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,4 +1,6 @@
+import { StatusCodes } from 'http-status-codes'
 import { boardModel } from '~/models/boardModel'
+import ApiError from '~/utils/ApiError'
 import { slugify } from '~/utils/formatters'
 
 const createNew = async (reqBody) => {
@@ -18,7 +20,20 @@ const createNew = async (reqBody) => {
     throw error
   }
 }
+const getDetails = async (boardId) => {
+  // eslint-disable-next-line no-useless-catch
+  try {
+    const board = await boardModel.getDetails(boardId)
+    if (!board) {
+      throw new ApiError(StatusCodes.NOT_FOUND, 'Board not found')
+    }
+    return board
+  } catch (error) {
+    throw error
+  }
+}
 
 export const boardService = {
-  createNew
+  createNew,
+  getDetails
 }
