5738ef94f8b4aea840f53f9b1d1f22ea493881a5
nguyenngochai2909 <nguyenngochai290902@gmail.com>
2025-04-16 08:30:40 +0700

    update white list domain

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const WHITELIST_DOMAINS = ['https://trello-web-lake-eight.vercel.app/']
+export const WHITELIST_DOMAINS = ['https://trello-web-lake-eight.vercel.app']
 
 export const BOARD_TYPE = {
   PUBLIC: 'public',
