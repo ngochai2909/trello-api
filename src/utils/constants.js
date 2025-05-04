d2c09e95dd4b7a181ee2e8ce650b5e82bd60b7d0
nguyenngochai2909 <nguyenngochai290902@gmail.com>
2025-05-05 00:35:48 +0700

    update verify account

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,13 @@
+import { env } from '~/config/environment'
+
 export const WHITELIST_DOMAINS = ['https://trello-web-lake-eight.vercel.app']
 
 export const BOARD_TYPE = {
   PUBLIC: 'public',
   PRIVATE: 'private'
 }
+
+export const WEBSITE_DOMAIN =
+  env.BUILD_MODE === 'production'
+    ? env.WEBSITE_DOMAIN_PROD
+    : env.WEBSITE_DOMAIN_DEV
