1713189fbbc68aeeef49c924ff27ecccbfcc1521
nguyenngochai2909 <nguyenngochai290902@gmail.com>
2025-05-05 11:05:35 +0700

    verify account

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -10,6 +10,26 @@ const createNew = async (req, res, next) => {
   }
 }
 
+const verifyAccount = async (req, res, next) => {
+  try {
+    const verifyUser = await userService.verifyAccount(req.body)
+    res.status(StatusCodes.OK).json(verifyUser)
+  } catch (error) {
+    next(error)
+  }
+}
+
+const login = async (req, res, next) => {
+  try {
+    const loginUser = await userService.login(req.body)
+    res.status(StatusCodes.OK).json(loginUser)
+  } catch (error) {
+    next(error)
+  }
+}
+
 export const userController = {
-  createNew
+  createNew,
+  verifyAccount,
+  login
 }
--- src/routes/v1/userRoute.js
+++ src/routes/v1/userRoute.js
@@ -8,6 +8,12 @@ Router.route('/register').post(
   userValidation.createNew,
   userController.createNew
 )
-// Router.route('/login').post(userValidation.login, userController.login)
+
+Router.route('/verify').put(
+  userValidation.verifyAccount,
+  userController.verifyAccount
+)
+
+Router.route('/login').post(userValidation.login, userController.login)
 
 export const userRoute = Router
